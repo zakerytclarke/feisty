02bf950bbe9c9c441907a433a260e0a29bfe27c9
Zakery Clarke <zakerytclarke@gmail.com>
2019-02-27 15:40:13 -0700

    New Format

--- generator.js
+++ generator.js
@@ -2,10 +2,10 @@ var n=1000000;
 var c=Math.random()*100;
 var v=Math.random()*10;
 
-var out=c;
+console.log("xValue,yValue");
+
 for(var i=0;i<n;i++){
-  out+=","+c;
+  console.log(i+","+c);
   c+=(Math.random()*2)-1;
   v+=(Math.random()*2)-1;
 }
-console.log(out);
